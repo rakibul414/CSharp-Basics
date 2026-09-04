57f61f1bc5848274c923f4d8cb2a2a3d918b26ce
Your Name <22235103414@cse.bubt.edu.bd>
2026-09-05 01:17:51 +0600

    solved problem 1. familiar with input output

--- input_function/Program.cs
+++ input_function/Program.cs
@@ -1,27 +1,4 @@
-﻿Console.Write("Enter Your Name  :  ");
+﻿problem1.run();
 
-string name = Console.ReadLine();
+Console.ReadKey();
 
-Console.Write("What is your age?  ");
-
-int age = int.Parse(Console.ReadLine());
-
-Console.Write("what is your blood group? ");
-
-string blood = Console.ReadLine();
-
-Console.Write("What is your weight? ");
-
-double weight = double.Parse(Console.ReadLine());
-
-Console.Write("Are you student?  ");
-
-string std = Console.ReadLine();
-
-std.ToLower();
-bool ok = (std == "yes");
-
-Console.WriteLine($"Hello {name}! You are {age} years old.\r\nBlood Group: {blood}, Weight: {weight} kg.\r\nStudent Status: {ok}");
-
-
-    
